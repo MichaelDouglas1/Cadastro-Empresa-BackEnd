df317b9fc004b38bf9d22ffa5bfd210222f7bdab
Michael D <michaeldouglas738@gmail.com>
2020-03-26 11:00:11 -0300

    removendo erro do CORS

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -18,10 +19,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import com.example.model.Empresa;
 import com.example.service.EmpresaService;
 
+@SuppressWarnings("deprecation")
 @SpringBootApplication(scanBasePackages = { "com.example.demo", "com.example.model", "com.example.rep",
 		"com.example.service" })
 @EntityScan("com.example.model")
@@ -81,4 +86,17 @@ public class DemoApplication {
 		return new ResponseEntity<Empresa>(empresaService.getById(id), new HttpHeaders(), HttpStatus.OK);
 	}
 
+	
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+            }
+        };
+    }
 }
+
+
